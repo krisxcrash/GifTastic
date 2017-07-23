dab8e01fa30ba8a6948c4ec1121f904b2311a9dd
Kristine Martin <kristinemartin@Kristines-MacBook-Pro.local>
2017-07-23 14:34:19 -0700

    updated javaScript; note giphy search buttons still not working

--- assets/app.js
+++ assets/app.js
@@ -1,33 +1,107 @@
+		var gifSearch = ["scooby doo", "shaggy rogers", "velma", "daphne"];
+
 $(document).ready(function() {
 
-	$("button").on("click", function() {
-		var person = $(this).attr("data-person");
+
+
+    	renderButtons();
+
+    // function to query the gifs on button click
+
+	$("button").on("click", function displayGiphy() {
+
+// assigns attribute to button for gif query
+
+
+		var gifButton = $(this).attr("data-person");
+
 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=dc6zaTOxFJmzC&limit=10";
+        gifButton + "&api_key=dc6zaTOxFJmzC&limit=10&rating=g";
 
     $.ajax({
     	url: queryURL,
-    	method: 'GET'
+    	method: "GET"
     })
     .done(function(response) {
     	var results = response.data;
 
     	for (var i = 0; i < results.length; i++) {
-    		var gifData = $("<div class='item'>");
+    		var gifDiv = $("<div class='item col-md-4'>");
 
     		var rating = results[i].rating;
 
     		var p = $("<p>").text("Rating: " + rating);
 
-    		var personImage = $("<img>");
-    		personImage.attr("src", results[i].images.fixed_height.url);
+    		var gifImage = $("<img>");
+    		gifImage.attr("src", results[i].images.fixed_width_still.url);
+    		gifImage.attr("data-still", results[i].images.fixed_width_still.url);
+    		gifImage.attr("data-animate", results[i].images.fixed_width.url);
+    		gifImage.attr("data-state", "still");
+    		gifImage.attr("height", "200");
+    		gifImage.attr("width", "300")
+    		gifImage.addClass("gif");
 
-    		gifDiv.html(p);
-    		gifDiv.html(personImage);
+    		gifDiv.prepend(p);
+    		gifDiv.prepend(gifImage);
 
-    		$("#gifsHere").html(gifDiv);
-    	}
-    })
+    		$("#gifsHere").prepend(gifDiv);
+    	};
 
-	})
-})
+    //if statement to pause and play gifs
+
+      $(".gif").on("click", function() {
+      	console.log("click")
+
+		var state = $(this).attr("data-state");
+        var animate = $(this).attr("data-animate");
+        var still = $(this).attr("data-still");
+
+      if (state === "still") {
+      	console.log("still")
+        $(this).attr("src", animate);
+        $(this).attr("data-state", "animate");
+      }
+      else {
+      	console.log("animate")
+        $(this).attr("src", still);
+        $(this).attr("data-state", "still");
+      };
+
+	});
+
+    });
+
+   });
+
+// function to add buttons when user searches
+
+    function renderButtons() {
+    	$("#buttons").empty();
+
+		for (var i = 0; i < gifSearch.length; i++) {
+
+			var a = $("<button>");
+			a.addClass("btn btn-default btn-lg btnDesign giphyButton");
+			a.attr("data-person", gifSearch[i]);
+			a.text(gifSearch[i]);
+
+			$("#buttons").append(a);
+		}	
+
+    }
+
+    $("#searchButton").on("click", function(event) {
+    	event.preventDefault();
+
+    	var gifButton = $("#searchInput").val().trim();
+
+    	gifSearch.push(gifButton);
+
+    	renderButtons();
+    });
+
+    $(document).on("click", ".giphyButton", displayGiphy);
+
+    renderButtons();
+
+});    
