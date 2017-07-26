8701599687157174704ae9b5094579f74397da8e
Kristine Martin <kristinemartin@Kristines-MacBook-Pro.local>
2017-07-25 20:56:04 -0700

    updated JavaScript to fix bug where gifs doubled

--- assets/app.js
+++ assets/app.js
@@ -1,92 +1,81 @@
 var topics = [];
 
+//runs when the page is ready
+$(document).on('click', '#searchButton', function() {
+
+})
 $(document).ready(function() {
 
-
-
-    	renderButtons();
+    renderButtons();
 	
-
-
-
-
-    // function to query the gifs on button click
+// function to query the gifs on button click
 	
-function bindClick(){
+	function bindClick(){
 
 	$("button").on("click", function() {
 
 // assigns attribute to button for gif query
         
-		var gifButton = $(this).attr("data-person");
+	var gifButton = $(this).attr("data-person");
 		
-		if(gifButton !== undefined){
-		 var q = gifButton;
+		if(gifButton !== undefined) {
+			var q = gifButton;
 		}
 		else{
-		 q = $("#searchInput").val();
+			q = $("#searchInput").val();
 		}		
 		
-		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        q + "&api_key=dc6zaTOxFJmzC&limit=10&rating=g";
-		
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + q + "&api_key=dc6zaTOxFJmzC&limit=10&rating=g";
 
-    $.ajax({
-    	url: queryURL,
-    	method: "GET"
-    })
-    .done(function(response) {
-    	var results = response.data;
+	    $.ajax({
+	    	url: queryURL,
+	    	method: "GET"
+	    })
+	    .done(function(response) {
+	    	var results = response.data;
 
-    	for (var i = 0; i < results.length; i++) {
-    		var gifDiv = $("<div class='item col-md-4 col-sm-6 squares'>");
+	    	for (var i = 0; i < results.length; i++) {
+	    		var gifDiv = $("<div class='item col-md-4 col-sm-6 squares'>");
 
-    		var rating = results[i].rating;
+	    		var rating = results[i].rating;
 
-    		var p = $("<p>").text("Rating: " + rating);
+	    		var p = $("<p>").text("Rating: " + rating);
 
-    		var gifImage = $("<img>");
-    		gifImage.attr("src", results[i].images.fixed_width_still.url);
-    		gifImage.attr("data-still", results[i].images.fixed_width_still.url);
-    		gifImage.attr("data-animate", results[i].images.fixed_width.url);
-    		gifImage.attr("data-state", "still");
-    		gifImage.attr("height", "300");
-    		gifImage.attr("width", "400")
-    		gifImage.addClass("gif");
+	    		var gifImage = $("<img>");
+	    		gifImage.attr("src", results[i].images.fixed_width_still.url);
+	    		gifImage.attr("data-still", results[i].images.fixed_width_still.url);
+	    		gifImage.attr("data-animate", results[i].images.fixed_width.url);
+	    		gifImage.attr("data-state", "still");
+	    		gifImage.attr("height", "300");
+	    		gifImage.attr("width", "400")
+	    		gifImage.addClass("gif");
 
-    		gifDiv.prepend(p);
-    		gifDiv.prepend(gifImage);
+	    		gifDiv.prepend(p);
+	    		gifDiv.prepend(gifImage);
 
-    		$("#gifsHere").prepend(gifDiv);
-    	};
+	    		$("#gifsHere").prepend(gifDiv);
+	    	};
 
-    //if statement to pause and play gifs
+//if statement to pause and play gifs
 
-      $(".gif").on("click", function() {
+		    $(".gif").on("click", function() {
 
-		var state = $(this).attr("data-state");
-        var animate = $(this).attr("data-animate");
-        var still = $(this).attr("data-still");
+				var state = $(this).attr("data-state");
+		    	var animate = $(this).attr("data-animate");
+		    	var still = $(this).attr("data-still");
 
-      if (state === "still") {
-
-        $(this).attr("src", animate);
-        $(this).attr("data-state", "animate");
-      }
-      else {
-
-        $(this).attr("src", still);
-        $(this).attr("data-state", "still");
-      };
-
+			    if (state === "still") {
+			    	$(this).attr("src", animate);
+			    	$(this).attr("data-state", "animate");
+			      }
+			    else {
+			    	$(this).attr("src", still);
+			    	$(this).attr("data-state", "still");
+			    };
+			});
+	    });
 	});
-
-    });
-
-   });
-}
-
-	bindClick();
+};
 	
 // function to add buttons when user searches
 
@@ -101,10 +90,12 @@ function bindClick(){
 			a.text(topics[i]);
 
 			$("#buttons").append(a);
-		}	
-		bindClick();
+		}
 
+		bindClick();
     }
+
+// adds search item to array
 
     $("#searchButton").on("click", function(event) {
     	event.preventDefault();
@@ -115,9 +106,5 @@ function bindClick(){
 
     	renderButtons();
     });
-
-    // $(document).on("click", ".giphyButton", displayGiphy);
-
-    // renderButtons();
 
 });    
